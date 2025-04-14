8300eba3f140769583cb12139036d4872c561976
Timur Kapitonov <kapitonov.tr@capitalgroupcorp.com>
2025-04-15 01:05:56 +0300

    some improvements

---
+++ task_manager.sh
@@ -0,0 +1,3 @@
+source ~/Documents/Python/Task-manager/.venv/bin/activate
+
+open http://127.0.0.1:8010/basic_app/tasks/ && python ~/Documents/Python/Task-manager/learning_users/manage.py runserver 0.0.0.0:8010
